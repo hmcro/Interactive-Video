e8c512693510e4f4db66b93c185ec818de84db9f
Luke <hello@lukesturgeon.co.uk>
2018-09-21 13:53:54 +0100

    removed unused of methods

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -256,11 +256,6 @@ void ofApp::playVideo(int n){
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-    
-}
-
-//--------------------------------------------------------------
 void ofApp::keyReleased(int key){
     
     if (key == 'a') {
@@ -361,45 +356,10 @@ void ofApp::removeVisitor(){
 char ofApp::getRandomChar(){
     
     return rand() % 26 + 'A';
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-    
-}
+} 
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
     
 }
 
